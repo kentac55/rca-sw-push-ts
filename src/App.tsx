c745bbc39133920f0cc18c4994f3b1f6965a393b
kentac55 <dev.kentac55@gmail.com>
2019-03-03 19:30:13 +0900

    add event buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import * as serviceWorker from './serviceWorker'
+import { TriggerAPI, TriggerSchema } from './endpoint'
 
 class App extends Component {
   public render(): JSX.Element {
@@ -19,10 +21,47 @@ class App extends Component {
           >
             Learn React
           </a>
+          <div>
+            <NotifyButton />
+            <UnregisterButton />
+          </div>
         </header>
       </div>
     )
   }
+}
+
+function NotifyButton(): JSX.Element {
+  function handleClick(ev: React.MouseEvent<HTMLElement>): void {
+    ev.preventDefault()
+    const msg: TriggerSchema = {
+      msg: new Date().toLocaleString(),
+    }
+    fetch(TriggerAPI, {
+      method: 'POST',
+      body: JSON.stringify(msg),
+    })
+    console.log('Just triggered server to publish notifications.')
+  }
+  return <button onClick={handleClick}>{'Notify'}</button>
+}
+
+// function RegisterButton(): JSX.Element {
+//   function handleClick(ev: React.MouseEvent<HTMLElement>): void {
+//     ev.preventDefault()
+//     serviceWorker.register()
+//     console.log('ServiceWorker registered')
+//   }
+//   return <button onClick={handleClick}>{'Register'}</button>
+// }
+
+function UnregisterButton(): JSX.Element {
+  function handleClick(ev: React.MouseEvent<HTMLElement>): void {
+    ev.preventDefault()
+    serviceWorker.unregister()
+    console.log('ServiceWorker unregistered')
+  }
+  return <button onClick={handleClick}>{'Unregister'}</button>
 }
 
 export default App
---
+++ src/endpoint.ts
@@ -0,0 +1,7 @@
+export const RegisterAPI = '/sw/register'
+export const TriggerAPI = '/sw/trigger'
+export const UnregisterAPI = '/sw/unregister'
+
+export interface TriggerSchema {
+  msg: string
+}
